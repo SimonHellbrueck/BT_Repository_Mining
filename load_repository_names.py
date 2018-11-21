3c4073506831fab5c4090d193505d80b1e078584
simon <simonhellbrueck@gmx.de>
2018-11-21 18:52:03 +0200

    update

--- load_repository_names.py
+++ load_repository_names.py
@@ -5,6 +5,17 @@ numberOfPages = 1
 input_java_repo = []
 input_kotlin_repo = []
 
+def getLifespanInDays(givenDate):
+    datetime_now = getDateFormat(datetime.datetime.today().strftime('%Y-%m-%d'))    
+    delta = datetime_now-givenDate
+    return delta.days
+
+def getDateFormat(givenDate):
+    year = int(givenDate[:-6])
+    month = int(givenDate[5:7])
+    day = int(givenDate[8:10])
+    return (date(year, month, day))
+
 for pageNumber in range(numberOfPages):
     url_java_repos = 'https://api.github.com/search/repositories?q=stars%3A%3E%3D10+language%3Ajava&sort=stars&order=desc&page='+str(pageNumber+1)+'&per_page=100'
     url_kotlin_repos = 'https://api.github.com/search/repositories?q=stars%3A%3E%3D10+language%3Akotlin&sort=stars&order=desc&page='+str(pageNumber+1)+'&per_page=100' 
@@ -16,11 +27,15 @@ for pageNumber in range(numberOfPages):
 with open('java_repository_names', 'a') as text_file:
     for pageNumber in range(len(input_java_repo)):
         for repo in input_java_repo[pageNumber]['items']:
-            text_file.write(repo['full_name'])
+            date_repo = repo['created_at'][:-10]            
+            lifespan = getLifespanInDays(getDateFormat(date_repo))
+            text_file.write(repo['full_name']+', '+str(lifespan))
             text_file.write('\n')
 
 with open('kotlin_repository_names', 'a') as text_file:
     for pageNumber in range(len(input_kotlin_repo)):
-        for repo in input_kotlin_repo[pageNumber]['items']:
-            text_file.write(repo['full_name'])
+        for repo in input_kotlin_repo[pageNumber]['items']:            
+            date_repo = repo['created_at'][:-10]
+            lifespan = getLifespanInDays(getDateFormat(date_repo))
+            text_file.write(repo['full_name']+', '+str(lifespan))
             text_file.write('\n')
