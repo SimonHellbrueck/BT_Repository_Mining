196a71965f406353dc223d134d69f909eb8ae81a
simon <simonhellbrueck@gmx.de>
2018-11-25 17:36:44 +0200

    update

--- load_repository_names.py
+++ load_repository_names.py
@@ -21,9 +21,11 @@ def user_spec_sample(sample_size):
     elif(sample_size == 400):
         return sample_size, 4
     elif(sample_size == 500):    
-        return sample_size, 4  
+        return sample_size, 4
+    elif(sample_size == 1000):    
+        return sample_size, 9
     else:
-        print("Please use 100, 200, 300, 400 or 500 as a sample size. Test case (sample size = 5) will be executed now.")
+        print("Please use 100, 200, 300, 400, 500 or 1000 as a sample size. Test case (sample size = 5) will be executed now.")
         return test_case()
 
 input_java_repo = []
@@ -46,18 +48,19 @@ try:
     number_of_pages = sample_size_and_pages[1]
     per_page = sample_size_and_pages[0]
 except IndexError:
-    print("No argument given. Please use 100, 200, 300, 400 or 500 as an argument for the sample size. Test case (sample size = 5) will be executed now.")
+    print("Please use 100, 200, 300, 400, 500 or 1000 as an argument for the sample size. Test case (sample size = 5) will be executed now.")
     sample_size_and_pages = test_case()
     number_of_pages = sample_size_and_pages[0]
     per_page = sample_size_and_pages[1]
        
-for page_number in range(number_of_pages):
+for page_number in range(1):
     url_java_repos = 'https://api.github.com/search/repositories?q=stars%3A%3E%3D10+language%3Ajava&sort=stars&order=desc&page='+str(page_number+1)+'&per_page='+str(per_page)
     url_kotlin_repos = 'https://api.github.com/search/repositories?q=stars%3A%3E%3D10+language%3Akotlin&sort=stars&order=desc&page='+str(page_number+1)+'&per_page='+str(per_page)
     r_java = requests.get(url_java_repos)
     r_kotlin = requests.get(url_kotlin_repos)
     input_java_repo.append(json.loads(r_java.text))
     input_kotlin_repo.append(json.loads(r_kotlin.text))
+    time.sleep(10)
 
 with open('java_repository_names', 'a') as text_file:
     for page_number in range(len(input_java_repo)):
